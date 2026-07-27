2b62286265d6020b3f2b9bb2da747af8b50f6488
Ousmaalk <ousmaaalk@gmail.com>
2026-07-27 10:14:06 +0200

    Correction couleur texte login

--- app/api/admin/articles/route.ts
+++ app/api/admin/articles/route.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Initialisation du client Supabase pour le storage
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL_2!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
